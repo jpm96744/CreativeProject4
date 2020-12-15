4acaea638450965083bc71417c55bdddbf28c375
Jordan Mack <jpm96744@gmail.com>
2020-12-14 23:17:09 -0700

    First Commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images',
+  limits: {
+    fileSize: 10000000
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,10 +17,86 @@ app.use(bodyParser.urlencoded({
 
 const mongoose = require('mongoose');
 
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  path: String,
+  numLikes: Number,
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
+mongoose.connect('mongodb://localhost:27017/memes', {
   useNewUrlParser: true
 });
 
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    description: req.body.description,
+    path: req.body.path,
+    numLikes: req.body.numLikes,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    let item = await Item.findOne({
+      _id: req.params.id
+    });
+    item.title = req.body.title;
+    item.description = req.body.description;
+    item.numLikes = req.body.numLikes;
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
